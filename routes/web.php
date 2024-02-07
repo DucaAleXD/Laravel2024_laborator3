82664d10c0ac229cdaf008cb1672ca7b24312bd7
DucaAleXD <ducasasha@gmail.com>
2024-02-07 08:09:00 +0200

    Update

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ Route::get('/users',[UserController::class,'index'])->name('users.index');
 Route::get('/users/create',[UserController::class,'create'])->name('users.create');
 Route::post('/users',[UserController::class,'store'])->name('users.store');
 
-Rote::controller(PhoneController::class)->group(function(){
+Route::controller(PhoneController::class)->group(function(){
     Route::get('phones','index');
     Route::get('phones/create','create');
     Route::post('phones','store');
